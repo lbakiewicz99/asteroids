7b3f4584cc20b426fc6be33cece19b4ada1bbb3a
lbakiewicz99 <lbakiewicz99@gmail.com>
2025-09-22 13:21:01 +0200

    Created shape class and setting FPS

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from constants import *
 def main():
     print(pygame.init())
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    new_clock = pygame.time.Clock()
+    dt = 0.00
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        new_clock.tick(60)
+        dt = new_clock.tick(60) / 1000
+        
 
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
